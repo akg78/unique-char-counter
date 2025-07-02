c7adae051804ea7c16b98349444c9c08e721b6cd
Ankit Kumar <134837821+akg78@users.noreply.github.com>
2025-07-02 15:27:37 +0530

    feat: added new stats features

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 function countUniqueChars() {
     const input = document.getElementById("userInput").value.toLowerCase();
+    const resultBox = document.getElementById("uniqueBox");
+
+    resultBox.classList.remove("hidden");
 
     if (!input.trim()) {
-        document.getElementById("uniqueBox").innerText =
-            "Please Enter Some Text!";
+        resultBox.innerText = "Please Enter Some Text!";
+        updateStats(0)
         return;
     }
 
     const cleanInput = input.toLowerCase().replace(/[^a-z]/g, '');
 
     if (!cleanInput) {
-        document.getElementById("uniqueBox").innerText =
-            "No Valid alphabetic characters!";
+        resultBox.innerText = "No Valid alphabetic characters!";
+        updateStats(0)
         return
     }
 
@@ -19,6 +22,20 @@ function countUniqueChars() {
     // const uniqueChars = [...uniqueCharCount].join(", ");
     // const count = uniqueChar.size;
 
-    document.getElementById("uniqueBox").innerText =
-        `Unique Characters Count: ${uniqueCharCount.size}`;
+    resultBox.innerText = `Unique Characters Count: ${uniqueCharCount.size}`;
+
+    const characters = input.length;
+    const words = input.trim().split(/\s+/).filter(Boolean).length
+    const sentences = input.split(/[.!?]+/).filter(s => s.trim().length > 0).length;
+    const paragraphs = input.split(/\n+/).filter(p => p.trim().length > 0).length;
+    const spaces = (input.match(/ /g) || []).length;
+    updateStats(characters, words, sentences, paragraphs, spaces)
+}
+
+function updateStats(characters, words, sentences, paragraphs, spaces) {
+    document.querySelector(".characters-count span").innerText = characters;
+    document.querySelector(".words-count span").innerText = words;
+    document.querySelector(".sentences-count span").innerText = sentences;
+    document.querySelector(".paragraphs-count span").innerText = paragraphs;
+    document.querySelector(".spaces-count span").innerText = spaces;
 }
